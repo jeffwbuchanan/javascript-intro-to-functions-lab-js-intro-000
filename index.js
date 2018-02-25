f916932e3569276562cbb3f83728cd1d6661f3ae
jeffwbuchanan@gmail.com <jeffwbuchanan@gmail.com>
2018-02-25 13:10:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+};
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log('HELLO')
+}
+
+function logWhisper(sting) {
+  console.log('hello')
+}
+
+function sayHiToGrandma(string) {
+  if (whisper = true) {
+  return ("I can't hear you!")
+} else if (shout === string.toUpperCase()) {
+  return ("YES INDEED!")
+}
+}
